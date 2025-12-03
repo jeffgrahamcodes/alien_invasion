e38dd5d46109b4076a75dd08852982826619e073
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-12-03 18:53:06 -0500

    refactor

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,14 +20,21 @@ class AlienInvasion:
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+
+    def _check_events(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
-            self.clock.tick(60)
+    def _update_screen(self):
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
